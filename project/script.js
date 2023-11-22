228cbde9ae175b4b42710220cdcbfcfd2bac6da6
Gabriel Henrique <xgabrielhenriquex@outlook.com>
2023-11-22 15:30:16 -0300

    informa-horas

---
+++ project/script.js
@@ -0,0 +1,19 @@
+function carregar()  {
+  let messageField = document.getElementById("message")
+  let imageField = document.getElementById("img")
+
+  let date = new Date()
+  let hour = date.getHours()
+  messageField.innerHTML = `Agora são ${hour} horas.`
+
+  if(hour >= 0 && hour < 12) {
+    imageField.src = "img/morning-circle.png"
+    document.body.style.background = "#F2C36B"
+  } else if(hour >= 12 && hour <= 18) {
+    imageField.src = "img/afternoon-circle.png"
+    document.body.style.background = "#3772A6"
+  } else {
+    imageField.src = "img/night-circle.png"
+    document.body.style.background = "#213740"
+  }
+}
